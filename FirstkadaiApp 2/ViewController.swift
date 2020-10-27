19afec715cbf70cc453545ccef06d79dc288f966
taiseitakai <taisei-0629-moco@ezweb.ne.jp>
2020-10-27 23:37:39 +0900

    Initial Commit

---
+++ FirstkadaiApp 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstkadaiApp 2
+//
+//  Created by 高井大政 on 2020/10/27.
+//  Copyright © 2020 taisei.takai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
